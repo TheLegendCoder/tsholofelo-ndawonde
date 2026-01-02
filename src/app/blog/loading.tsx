340336b031e669539292173f489c30dd0b9c1c04
Tsholofelo <hopendawonde@gmail.com>
2026-01-02 18:27:30 +0200

    chore: remove unused loading components and outdated guides for improved codebase clarity

--- src/app/blog/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { PageLoading } from '@/components/ui/loading-spinner';
-
-export default function Loading() {
-  return <PageLoading message="Loading blog posts..." className="min-h-[400px]" />;
-}
